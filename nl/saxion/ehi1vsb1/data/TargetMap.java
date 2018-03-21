21420ade8819e7446c7d75c5a11454e43dda9ed8
Thymo van Beers <thymovanbeers@gmail.com>
2018-03-21 14:13:32 +0100

    Basic target map

--- nl/saxion/ehi1vsb1/data/TargetMap.java
+++ nl/saxion/ehi1vsb1/data/TargetMap.java
@@ -1,10 +1,34 @@
+package nl.saxion.ehi1vsb1.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Map containing targets
+ *
+ * @author Thymo van Beers
+ */
 public class TargetMap {
-    private List<Target> targetMap;
+    private List<Target> targetList;
+
+    public TargetMap() {
+        targetList = new ArrayList<>();
+    }
 
     /**
+     * Find the target closest to the given position
      *
+     * @param xPos X position
+     * @param yPos Y position
+     *
+     * @return The closest target
      */
     public Target getClosest(double xPos, double yPos) {
-        
+        //TODO: Find closest target based on given position
+        return null;
+    }
+
+    public void addTarget(Target target) {
+        targetList.add(target);
     }
 }
