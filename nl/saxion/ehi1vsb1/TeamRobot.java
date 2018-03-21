2a373b560bdc797a27ea9a5726b963901895180e
Thymo van Beers <thymovanbeers@gmail.com>
2018-03-21 13:22:13 +0100

    Add steerTo method (synchronous)

--- nl/saxion/ehi1vsb1/TeamRobot.java
+++ nl/saxion/ehi1vsb1/TeamRobot.java
@@ -1,5 +1,7 @@
 package nl.saxion.ehi1vsb1;
 
+import robocode.TurnCompleteCondition;
+
 abstract public class TeamRobot extends robocode.TeamRobot {
 
     /**
@@ -33,5 +35,28 @@ abstract public class TeamRobot extends robocode.TeamRobot {
         double dY = ycmd - getY();
 
         return (Math.sqrt((dX*dX+dY*dY)));
+    }
+
+    /**
+     * Given a heading calculate the steering angle
+     * and steer to it
+     *
+     * @param heading Desired heading (north referenced)
+     * @return The calculated steering angl
+     *
+     * @author Thymo van Beers
+     */
+    protected double steerTo(double heading) {
+        double steerAngle = heading - getHeading();
+
+        if (steerAngle < -180) {
+            steerAngle = 360 - -steerAngle;
+        }
+
+        out.println("Steer command: " + steerAngle);
+
+        setTurnRight(steerAngle);
+        waitFor(new TurnCompleteCondition(this));
+        return steerAngle;
     }
 }
