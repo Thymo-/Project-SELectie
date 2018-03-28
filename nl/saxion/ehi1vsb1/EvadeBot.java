87288115362579b9e7a4b94223e1194898de7cb7
Sieger van Breugel <32195464+siegerbreugel@users.noreply.github.com>
2018-03-28 14:24:36 +0200

    Customized onScannedRobot event

--- nl/saxion/ehi1vsb1/EvadeBot.java
+++ nl/saxion/ehi1vsb1/EvadeBot.java
@@ -3,16 +3,32 @@ package nl.saxion.ehi1vsb1;
 import robocode.ScannedRobotEvent;
 
 public class EvadeBot extends TeamRobot {
+    /**
+     * Main run method
+     *
+     * @author Sieger van Breugel
+     */
     @Override
     public void run() {
         super.run();
 
-        turnRadarLeft(360);
-
+        turnRadarLeft(Double.POSITIVE_INFINITY);
     }
 
     @Override
     public void onScannedRobot(ScannedRobotEvent event) {
         super.onScannedRobot(event);
+        turnRadarLeft(currentTarget.getHeading());
+
+        double energy = currentTarget.getEnergy();
+        while (currentTarget.getEnergy() > 0) {
+            scan();
+            if (currentTarget.getEnergy() < energy) {
+                super.evade(currentTarget);
+            }
+            else {
+                fire(100);
+            }
+        }
     }
 }
