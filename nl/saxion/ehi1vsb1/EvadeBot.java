ca9b5ac1ce837a34ee8c923e2eabb18a4f2478ab
Sieger van Breugel <32195464+siegerbreugel@users.noreply.github.com>
2018-03-28 12:29:58 +0200

    Overridden the run and onScannedRobot classes

--- nl/saxion/ehi1vsb1/EvadeBot.java
+++ nl/saxion/ehi1vsb1/EvadeBot.java
@@ -1,5 +1,18 @@
 package nl.saxion.ehi1vsb1;
 
+import robocode.ScannedRobotEvent;
+
 public class EvadeBot extends TeamRobot {
-    
+    @Override
+    public void run() {
+        super.run();
+
+        turnRadarLeft(360);
+
+    }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent event) {
+        super.onScannedRobot(event);
+    }
 }
