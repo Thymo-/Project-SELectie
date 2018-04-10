3bb9e40543219f76dfc5d25172ce4f0879862e90
Thymo van Beers <thymovanbeers@gmail.com>
2018-04-11 00:02:18 +0200

    AttackBot: Very basic radar heartbeat

--- nl/saxion/ehi1vsb1/AttackBot.java
+++ nl/saxion/ehi1vsb1/AttackBot.java
@@ -1,14 +1,35 @@
 package nl.saxion.ehi1vsb1;
 
+import robocode.ScannedRobotEvent;
+
 /**
  * AttackBot
  *
  * @author Thymo van Beers
  */
 public class AttackBot extends TeamRobot {
+    private boolean firstBotDetected;
 
     @Override
     public void run() {
         super.run();
+
+        while (!firstBotDetected)
+            doNothing();
+
+        // Logic loop
+        while (true) {
+            radarStep();
+
+            execute();
+        }
+    }
+
+    @Override
+    public void onScannedRobot(ScannedRobotEvent event) {
+        super.onScannedRobot(event);
+
+        if (!firstBotDetected)
+            firstBotDetected = true;
     }
 }
