1601f7661fa547329c13453389a51c85e7ddcc86
Thymo van Beers <thymovanbeers@gmail.com>
2018-04-10 23:29:26 +0200

    AttackBot: Initial commit

---
+++ nl/saxion/ehi1vsb1/AttackBot.java
@@ -0,0 +1,14 @@
+package nl.saxion.ehi1vsb1;
+
+/**
+ * AttackBot
+ *
+ * @author Thymo van Beers
+ */
+public class AttackBot extends TeamRobot {
+
+    @Override
+    public void run() {
+        super.run();
+    }
+}
